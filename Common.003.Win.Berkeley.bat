d7dbb51c65c6459ea06d219a892b5dbfda255b1b
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-29 16:03:43 +0800

    1.0.2
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- Common.003.Win.Berkeley.bat
+++ Common.003.Win.Berkeley.bat
@@ -4,7 +4,7 @@ cd .\bin
 
 rd /s/q .\org\hy\common\berkeley\junit
 
-jar cvfm hy.common.berkeley.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.berkeley.jar MANIFEST.MF META-INF org
 
 copy hy.common.berkeley.jar ..
 del /q hy.common.berkeley.jar
--- Common.004.Mac.Berkeley.sh
+++ Common.004.Mac.Berkeley.sh
@@ -4,7 +4,7 @@ cd ./bin
 
 rm -R ./org/hy/common/berkeley/junit
 
-jar cvfm hy.common.berkeley.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.berkeley.jar MANIFEST.MF META-INF org
 
 cp hy.common.berkeley.jar ..
 rm hy.common.berkeley.jar
