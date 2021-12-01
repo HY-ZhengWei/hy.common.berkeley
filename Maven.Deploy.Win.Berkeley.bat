aba78d8938ca5dbd8db9c6575462774c227cd56b
ZhengWei <HY.ZhengWei@qq.com>
2021-12-01 15:54:50 +0800

    1.0.3
    1. 调整：动态化Maven服务器的地址

--- Maven.Deploy.Win.Berkeley.bat
+++ Maven.Deploy.Win.Berkeley.bat
@@ -1,2 +1,2 @@
-start mvn deploy:deploy-file -Dfile=hy.common.berkeley.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
-start mvn deploy:deploy-file -Dfile=hy.common.berkeley-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.berkeley.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.berkeley-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
