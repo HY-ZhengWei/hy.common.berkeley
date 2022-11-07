b813e68c78ab7687cc860808d185b16b33deba7c
ZhengWei <HY.ZhengWei@qq.com>
2022-11-07 17:19:12 +0800

    1.0.4
    1. 完善：工程外围配置及环境

--- Maven.Deploy.Mac.Berkeley.sh
+++ Maven.Deploy.Mac.Berkeley.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-mvn deploy:deploy-file -Dfile=hy.common.berkeley.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
-mvn deploy:deploy-file -Dfile=hy.common.berkeley-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
+mvn install:install-file -Dfile=hy.common.berkeley.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.berkeley/pom.xml
+mvn install:install-file -Dfile=hy.common.berkeley-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.berkeley/pom.xml
+
+mvn deploy:deploy-file   -Dfile=hy.common.berkeley.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=hy.common.berkeley-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.berkeley/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
