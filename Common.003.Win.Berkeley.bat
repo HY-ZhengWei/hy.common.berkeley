1b1577144f3d511920b466078c546534b936ad14
ZhengWei <HY.ZhengWei@qq.com>
2022-11-07 14:27:24 +0800

    1.0.4
    1. 添加：支持IDEA和Eclipse双编译器
    2. 添加：支持向私库发版 org
    3. 添加：支持向公共库发版 cn.openapis

--- Common.003.Win.Berkeley.bat
+++ Common.003.Win.Berkeley.bat
@@ -1,23 +1,29 @@
 
 
-cd .\bin
+del /Q hy.common.berkeley.jar
+del /Q hy.common.berkeley-sources.jar
 
+
+call mvn clean package
+cd .\target\classes
 
 rd /s/q .\org\hy\common\berkeley\junit
 
 
-jar cvfm hy.common.berkeley.jar MANIFEST.MF META-INF org
+jar cvfm hy.common.berkeley.jar META-INF/MANIFEST.MF META-INF org
 
-copy hy.common.berkeley.jar ..
+copy hy.common.berkeley.jar ..\..
 del /q hy.common.berkeley.jar
-cd ..
+cd ..\..
 
 
 
 
 
-cd .\src
-jar cvfm hy.common.berkeley-sources.jar MANIFEST.MF META-INF org 
-copy hy.common.berkeley-sources.jar ..
-del /q hy.common.berkeley-sources.jar
-cd ..
+cd .\src\main\java
+xcopy /S ..\resources\* .
+jar cvfm hy.common.berkeley-sources.jar META-INF\MANIFEST.MF META-INF org 
+copy hy.common.berkeley-sources.jar ..\..\..
+del /Q hy.common.berkeley-sources.jar
+rd /s/q META-INF
+cd ..\..\..
